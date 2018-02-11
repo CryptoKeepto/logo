ae8ba42524a977fc3bc9be5b9f8edb0ee2a47d42
sittikiat <miikeguru@gmail.com>
2018-02-11 22:06:32 +0700

    route success

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,56 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { CategoryComponent } from './category/category.component';
+import { BestsellerComponent } from './bestseller/bestseller.component';
+import { NewsComponent } from './news/news.component';
+import { LoginComponent } from './login/login.component';
+import { SlideComponent } from './slide/slide.component';
+import { ProductComponent } from './product/product.component';
+import { ProductDetailComponent } from './product/product-detail/product-detail.component';
+import { StaffComponent } from './staff/staff.component';
+import { AboutComponent } from './about/about.component';
+import { NewsallComponent } from './newsall/newsall.component';
+import { NewsService } from "./shared/news.service";
 
+const routes: Routes = [
+  { path: '', component: NewsComponent },
+  { path: 'product', component: ProductComponent },
+  { path: 'product/:id', component: ProductDetailComponent },
+  { path: 'newsAll', component: NewsallComponent },
+  { path: 'staff', component: StaffComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '**', redirectTo: '' }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CategoryComponent,
+    BestsellerComponent,
+    NewsComponent,
+    LoginComponent,
+    SlideComponent,
+    ProductComponent,
+    ProductDetailComponent,
+    StaffComponent,
+    AboutComponent,
+    NewsallComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes, { useHash: true })
   ],
-  providers: [],
+  providers: [
+    Title,
+    NewsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
